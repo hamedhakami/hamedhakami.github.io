9651626f1d246c8ae5b413e01147980b34691ae8
hamedhakami <hamed.hakami@gmail.com>
2023-09-28 22:35:52 +0330

    v1.04

--- Beta/scripts.js
+++ Beta/scripts.js
@@ -92,13 +92,13 @@ $(document).on("scroll", function() {
 
   for (var i = 0; i < tags.length; i++) {
     var tag = tags[i];
-    if (($(tag).position().top < pageBottom) && ($(tag).position().bottom > pageTop) && (aa == 0) ) {
-      refreshIframe1();
-		console.log ("visible");
-	  var aa = 1;	
+    if (($(tag).position().top < pageBottom) && ($(tag).position().bottom > pageTop) && ($( "#getyourmoneyback" ).hasClass( "invisible" ) = true; )) {
+		refreshIframe1();
+      $(tag).removeClass("invisible");
+		console.log("visible")
     } else {
-		var aa = 0;
-		console.log ("invisible");
+      $(tag).addClass("invisible");
+		console.log("invisible")
     }
   }
 });
