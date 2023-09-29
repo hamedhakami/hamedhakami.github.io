5faea60f99e636a58e0bed71e902d74c1b4d955e
hamedhakami <hamed.hakami@gmail.com>
2023-09-29 15:09:31 +0330

    Update scripts.js

--- Beta/scripts.js
+++ Beta/scripts.js
@@ -91,7 +91,7 @@ $(document).on("scroll", function() {
 
   for (var i = 0; i < tags.length; i++) {
     var tag = tags[i];
-    if (($(tag).position().top <= pageBottom) && ($(tag).position().bottom >= pageTop) && (visibility < 1)) {
+    if (($(tag).position().top <= pageBottom) && (visibility < 1)) {
       refreshIframe1();
 	  visibility = 1;
 	  console.log("visible")
@@ -102,9 +102,10 @@ $(document).on("scroll", function() {
 	  if ((($(tag).position().top > pageBottom) || ($(tag).position().bottom < pageTop)) && (visibility == 1)) {
       
 	  visibility = 0;
+		  console.log("invisible2")
 	  
     } else {
-      
+      console.log("visible2")
 	  
     }
   }
