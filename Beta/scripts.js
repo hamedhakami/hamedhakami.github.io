c1ca0f4567caa885a5c73149ea6145767c830cd9
hamedhakami <hamed.hakami@gmail.com>
2023-10-03 17:58:27 +0330

    Update scripts.js

--- Beta/scripts.js
+++ Beta/scripts.js
@@ -77,7 +77,8 @@ const slider = document.querySelector('.items');
 
 
 
-
+  var tags = $(".tag");
+  const visibilityCheckers = new Array(tags.length);
 	
 	
 	
@@ -85,7 +86,7 @@ $(document).on("scroll", function() {
   var pageTop = $(document).scrollTop();
   var pageBottom = pageTop + $(window).height();
   var tags = $(".tag");
-  const visibilityCheckers = new Array(tags.length);
+  
 
   for (var i = 0; i < tags.length; i++) {
 	  
