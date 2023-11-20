73e1550a96e1527044e0ff3a204d0a01c6ff9928
hamedhakami <77845168+hamedhakami@users.noreply.github.com>
2023-11-20 17:24:14 +0330

    V2.2.5
    Some classes and variables were refactored.

--- scripts.js
+++ scripts.js
@@ -1,45 +1,45 @@
 $(document).ready(function () {
 
 
-    var items = $(".grabbing-effect-caousel");
-    for (var x = 0; x < items.length; x++) {
+    var carousels = $(".grabbing-effect-caousel");
+    for (var x = 0; x < carousels.length; x++) {
 
 
-        const slider = items[x];
+        const carousel = carousels[x];
         let isDown = false;
         let startX;
         let scrollLeft;
 
-        slider.addEventListener('mousedown', (e) => {
+        carousel.addEventListener('mousedown', (e) => {
             isDown = true;
-            slider.classList.add('active');
-            startX = e.pageX - slider.offsetLeft;
-            scrollLeft = slider.scrollLeft;
+            carousel.classList.add('active');
+            startX = e.pageX - carousel.offsetLeft;
+            scrollLeft = carousel.scrollLeft;
             cancelMomentumTracking();
         });
 
 
-        slider.addEventListener('mouseleave', () => {
+        carousel.addEventListener('mouseleave', () => {
             isDown = false;
-            slider.classList.remove('active');
+            carousel.classList.remove('active');
         });
 
 
-        slider.addEventListener('mouseup', () => {
+        carousel.addEventListener('mouseup', () => {
             isDown = false;
-            slider.classList.remove('active');
+            carousel.classList.remove('active');
             beginMomentumTracking();
         });
 
 
-        slider.addEventListener('mousemove', (e) => {
+        carousel.addEventListener('mousemove', (e) => {
             if (!isDown) return;
             e.preventDefault();
-            const x = e.pageX - slider.offsetLeft;
+            const x = e.pageX - carousel.offsetLeft;
             const walk = (x - startX) * 1; //scroll-fast
-            var prevScrollLeft = slider.scrollLeft;
-            slider.scrollLeft = scrollLeft - walk;
-            velX = slider.scrollLeft - prevScrollLeft;
+            var prevScrollLeft = carousel.scrollLeft;
+            carousel.scrollLeft = scrollLeft - walk;
+            velX = carousel.scrollLeft - prevScrollLeft;
         });
 
         // Momentum 
@@ -47,7 +47,7 @@ $(document).ready(function () {
         var velX = 0;
         var momentumID;
 
-        slider.addEventListener('wheel', (e) => {
+        carousel.addEventListener('wheel', (e) => {
             cancelMomentumTracking();
         });
 
@@ -61,7 +61,7 @@ $(document).ready(function () {
         }
 
         function momentumLoop() {
-            slider.scrollLeft += velX;
+            carousel.scrollLeft += velX;
             velX *= 0.95;
             if (Math.abs(velX) > 0.5) {
                 momentumID = requestAnimationFrame(momentumLoop);
@@ -97,8 +97,8 @@ $(document).ready(function () {
     })
 
 
-    var tileContainers = $(".tile-container");
-    const visibilityCheckers = new Array(tileContainers.length);
+    var iframeContainers = $(".html-container");
+    const reloadIframeCounter = new Array(iframeContainers.length);
 
 
     Scroll.addListener((s) => {
@@ -123,150 +123,149 @@ $(document).ready(function () {
         }
     }
 
-    // Here's a dummy function that we want to throttle 
     function runOnScroll() {
 
 
         var pageTop = Scroll.offset.y;
         var pageBottom = pageTop + $(window).height();
-        var tileContainers = $(".tile-container");
-        var tileEffects = $(".html-canvas");
+        var iframeContainers = $(".html-container");
+        var iframes = $(".html-canvas");
 
         //console.log("Page bottom:" + pageBottom)
         var windowHeight = $(window).height();
         //console.log("Viewpoert height:" + windowHeight);
 
-        var tags = $(".opacity-effect");
+        var opacityEffectObjects = $(".opacity-effect");
 
-        for (var i = 0; i < tileContainers.length; i++) {
+        for (var i = 0; i < iframeContainers.length; i++) {
 
-            var tileContainer = tileContainers[i];
-            var tileEffectObject = tileEffects[i];
+            var iframeContainer = iframeContainers[i];
+            var iframeTarget = iframes[i];
             var scrollContainers = $(".scroll-container");
             var mainScrollContainer = scrollContainers[0];
             const mainScrollBar = Scrollbar.init(mainScrollContainer);
-            var isVisibleChecker = mainScrollBar.isVisible(tileEffectObject);
-            //console.log ("Iframe " + i + " visibility: " + isVisibleChecker);
+            var isVisibleCheckerForIframes = mainScrollBar.isVisible(iframeTarget);
+            //console.log ("Iframe " + i + " visibility: " + isVisibleCheckerForIframes);
 
 
             var arrayOfFunction = [
 
                 function () {
-                    tileEffectObject.src = "iframes/Introduction Text/1.html";
+                    iframeTarget.src = "iframes/Introduction Text/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Nowruz/Describtions/1.html";
+                    iframeTarget.src = "iframes/Nowruz/Describtions/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Nowruz/Visuals/1.html";
+                    iframeTarget.src = "iframes/Nowruz/Visuals/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Nowruz/Sliders/1.html";
+                    iframeTarget.src = "iframes/Nowruz/Sliders/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Get Your Money Back/Describtion/1.html";
+                    iframeTarget.src = "iframes/Get Your Money Back/Describtion/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Get Your Money Back/Visual/1 v6.html";
+                    iframeTarget.src = "iframes/Get Your Money Back/Visual/1 v6.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Mini-Campaigns/Describtions/1.html";
+                    iframeTarget.src = "iframes/Mini-Campaigns/Describtions/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Mini-Campaigns/Visual/1 v6.html";
+                    iframeTarget.src = "iframes/Mini-Campaigns/Visual/1 v6.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/As Vast As Iran/Describtion/1.html";
+                    iframeTarget.src = "iframes/As Vast As Iran/Describtion/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/As Vast As Iran/Visual/1.html";
+                    iframeTarget.src = "iframes/As Vast As Iran/Visual/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Web Sliders/Describtion/1.html";
+                    iframeTarget.src = "iframes/Web Sliders/Describtion/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Web Sliders/Visual/1.html";
+                    iframeTarget.src = "iframes/Web Sliders/Visual/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Charity/Describtions/1.html";
+                    iframeTarget.src = "iframes/Charity/Describtions/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Charity/Visuals/1.html";
+                    iframeTarget.src = "iframes/Charity/Visuals/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/The Trace of Brightness/Describtions/1.html";
+                    iframeTarget.src = "iframes/The Trace of Brightness/Describtions/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/The Trace of Brightness/Visuals/1.html";
+                    iframeTarget.src = "iframes/The Trace of Brightness/Visuals/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/The Trace of Brightness/Visuals2/1.html";
+                    iframeTarget.src = "iframes/The Trace of Brightness/Visuals2/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Derby/Describtions/1.html";
+                    iframeTarget.src = "iframes/Derby/Describtions/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Derby/Visuals/1.html";
+                    iframeTarget.src = "iframes/Derby/Visuals/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Chess/Visuals/1.html";
+                    iframeTarget.src = "iframes/Chess/Visuals/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Marhaba/Describtions/1.html";
+                    iframeTarget.src = "iframes/Marhaba/Describtions/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Marhaba/Visuals/1.html";
+                    iframeTarget.src = "iframes/Marhaba/Visuals/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Taaghche/Describtions/1.html";
+                    iframeTarget.src = "iframes/Taaghche/Describtions/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Taaghche/Visuals/1.html";
+                    iframeTarget.src = "iframes/Taaghche/Visuals/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Live Wallpapers 4K/Describtion/1.html";
+                    iframeTarget.src = "iframes/Live Wallpapers 4K/Describtion/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Live Wallpapers 4K/Visual/1.html";
+                    iframeTarget.src = "iframes/Live Wallpapers 4K/Visual/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Live Wallpapers 4K/Visual2/1.html";
+                    iframeTarget.src = "iframes/Live Wallpapers 4K/Visual2/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Yalda99/Describtion/1.html";
+                    iframeTarget.src = "iframes/Yalda99/Describtion/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Yalda99/Visual/slider.html";
+                    iframeTarget.src = "iframes/Yalda99/Visual/slider.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Puzzle/Visuals/1.html";
+                    iframeTarget.src = "iframes/Puzzle/Visuals/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Sibche Design System/Describtion/1.html";
+                    iframeTarget.src = "iframes/Sibche Design System/Describtion/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Sibche Design System/Visuals/1.html";
+                    iframeTarget.src = "iframes/Sibche Design System/Visuals/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Sibche Social Media Contents/Describtion/1.html";
+                    iframeTarget.src = "iframes/Sibche Social Media Contents/Describtion/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Sibche Social Media Contents/Visual/1.html";
+                    iframeTarget.src = "iframes/Sibche Social Media Contents/Visual/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Sibche HTML Ads/Describtion/1.html";
+                    iframeTarget.src = "iframes/Sibche HTML Ads/Describtion/1.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Sibche HTML Ads/sibche1401/wheel.html";
+                    iframeTarget.src = "iframes/Sibche HTML Ads/sibche1401/wheel.html";
                 },
                 function () {
-                    tileEffectObject.src = "iframes/Sibche HTML Ads/sibchehtmlads/300X250.html";
+                    iframeTarget.src = "iframes/Sibche HTML Ads/sibchehtmlads/300X250.html";
                 },
             ]
 
 
             function refreshIframe() {
-                tileEffectObject.src = tileEffectObject.src;
+                iframeTarget.src = iframeTarget.src;
             }
 
             function loadIframe() {
@@ -276,14 +275,14 @@ $(document).ready(function () {
 
             function clearIframe() {
 
-                tileEffectObject.src = "blank.html";
+                iframeTarget.src = "blank.html";
             }
 
-            //console.log("Iframe" + i + " top position:" + $(tileContainer).position().top)
+            //console.log("Iframe" + i + " top position:" + $(iframeContainer).position().top)
 
-            if ((isVisibleChecker == true) && (visibilityCheckers[i] < 1)) {
+            if ((isVisibleCheckerForIframes == true) && (reloadIframeCounter[i] < 1)) {
                 loadIframe();
-                visibilityCheckers[i] = 1;
+                reloadIframeCounter[i] = 1;
                 //console.log("-------------------------------------------------------- Iframe " + i + " has been reloaded ---------------------------------------------------")
             } else {
 
@@ -291,31 +290,31 @@ $(document).ready(function () {
             }
 
 
-            if ((isVisibleChecker == false) && (visibilityCheckers[i] == 1)) {
+            if ((isVisibleCheckerForIframes == false) && (reloadIframeCounter[i] == 1)) {
 
-                visibilityCheckers[i] = 0;
+                reloadIframeCounter[i] = 0;
                 clearIframe();
                 //console.log("iframe " + i +" has gone out from the viewport and therefore has been cleared")
             } else {
-                visibilityCheckers[i] = 1;
+                reloadIframeCounter[i] = 1;
 
                 //console.log("iframe " + i +" is in viewport, but has not to be reloaded; because it was reloaded once.")
             }
 
 
-            if (isVisibleChecker == false) {
+            if (isVisibleCheckerForIframes == false) {
 
-                visibilityCheckers[i] = 0;
+                reloadIframeCounter[i] = 0;
 
             } else {};
 
         };
 
-        var tags = $(".opacity-effect");
+        var opacityEffectObjects = $(".opacity-effect");
 
-        for (var j = 0; j < tags.length; j++) {
+        for (var j = 0; j < opacityEffectObjects.length; j++) {
 
-            var tag = tags[j];
+            var OpacityEffectTarget = opacityEffectObjects[j];
 
             //var scrollContainers = $(".scroll-container");
             //var mainScrollContainer = scrollContainers[0];
@@ -323,14 +322,14 @@ $(document).ready(function () {
             var scrollContainers = $(".scroll-container");
             var mainScrollContainer = scrollContainers[0];
             const mainScrollBar = Scrollbar.init(mainScrollContainer);
-            var isVisibleCheckerForTags = mainScrollBar.isVisible(tag);
-            //console.log ("Tag " + j + " visibility: " + isVisibleCheckerForTags);
+            var isVisibleCheckerForOpacityEffect = mainScrollBar.isVisible(OpacityEffectTarget);
+            //console.log ("Object " + j + " visibility: " + isVisibleCheckerForOpacityEffect);
 
 
-            if (isVisibleCheckerForTags == true) {
-                $(tag).addClass("visible");
+            if (isVisibleCheckerForOpacityEffect == true) {
+                $(OpacityEffectTarget).addClass("visible");
             } else {
-                $(tag).removeClass("visible");
+                $(OpacityEffectTarget).removeClass("visible");
             }
         };
 
